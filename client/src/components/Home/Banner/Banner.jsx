d67df9ac90e9cb4c3f72f9bb21da0989b3d3cf3e
Atif <atifarshad0315@gmail.com>
2024-09-27 12:11:27 +0500

    complete hero banner section

--- client/src/components/Home/Banner/Banner.jsx
+++ client/src/components/Home/Banner/Banner.jsx
@@ -1,6 +1,24 @@
 import "./Banner.scss";
+import BannerImg from "../../../assets/banner-img.png";
 const Banner = () => {
-    return <div>Banner</div>;
+  return (
+    <div className="hero-banner">
+      <div className="content">
+        <div className="text-content">
+          <h1>SALES</h1>
+          <p>
+            Convallis interdum purus adipiscing dis parturient posuere ac quam a
+            eleifed montes parturient posuere curae tempor
+          </p>
+          <div className="ctas">
+            <div className="banner-cta">Shop Now</div>
+            <div className="banner-cta v2">Learn More</div>
+          </div>
+        </div>
+        <img src={BannerImg} alt="banner" className="banner-img" />
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
