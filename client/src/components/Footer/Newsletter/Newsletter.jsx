e864bd8817f87ea6968cafc6694c6e6a25ebb281
Atif <atifarshad0315@gmail.com>
2024-09-28 23:58:10 +0500

    Make newsletter section

--- client/src/components/Footer/Newsletter/Newsletter.jsx
+++ client/src/components/Footer/Newsletter/Newsletter.jsx
@@ -1,6 +1,37 @@
+import React from "react";
 import "./Newsletter.scss";
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
+
 const Newsletter = () => {
-    return <div>Newsletter</div>;
+  return (
+    <div className="newsletter-section">
+      <div className="newsletter-content">
+        <span className="small-text">Newsletter</span>
+        <span className="big-text">Sign up for latest updates and offers</span>
+        <div className="form">
+          <input type="text" placeholder="Email Address" />
+          <button>Subscribe</button>
+        </div>
+        <div className="text">
+          Will be used in accordance with our Privacy Policy
+        </div>
+        <div className="social-icon">
+          <div className="icon">
+            <FaFacebook size={25} color="#3b5998" />
+          </div>
+          <div className="icon">
+            <FaTwitter size={25} color="#55acee" />
+          </div>
+          <div className="icon">
+            <FaInstagram size={25} color="#F56040" />
+          </div>
+          <div className="icon">
+            <FaLinkedin size={25} color="#007bb6" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Newsletter;
