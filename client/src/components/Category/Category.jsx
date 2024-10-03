fb08579676de7aa0069517258718ba6f3263e655
Atif <atifarshad0315@gmail.com>
2024-10-03 12:57:23 +0500

    work on category page

--- client/src/components/Category/Category.jsx
+++ client/src/components/Category/Category.jsx
@@ -1,25 +1,12 @@
+import Products from "../Products/Products";
 import "./Category.scss";
-import cate1 from "../../assets/category/cat-1.jpg";
-import cate2 from "../../assets/category/cat-2.jpg";
-import cate3 from "../../assets/category/cat-3.jpg";
-import cate4 from "../../assets/category/cat-4.jpg";
 
 const Category = () => {
   return (
-    <div className="shop-by-category">
-      <div className="categories">
-        <div className="catgeory">
-          <img src={cate1} alt="category" />
-        </div>
-        <div className="catgeory">
-          <img src={cate2} alt="category" />
-        </div>
-        <div className="catgeory">
-          <img src={cate3} alt="category" />
-        </div>
-        <div className="catgeory">
-          <img src={cate4} alt="category" />
-        </div>
+    <div className="category-main-content">
+      <div className="layout">
+        <div className="category-title">Category Title</div>
+        <Products innerPage={true} />
       </div>
     </div>
   );
--- client/src/components/Home/Category/Category.jsx
+++ client/src/components/Home/Category/Category.jsx
@@ -1,6 +1,28 @@
 import "./Category.scss";
+import cate1 from "../../../assets/category/cat-1.jpg";
+import cate2 from "../../../assets/category/cat-2.jpg";
+import cate3 from "../../../assets/category/cat-3.jpg";
+import cate4 from "../../../assets/category/cat-4.jpg";
+
 const Category = () => {
-    return <div>Category</div>;
+  return (
+    <div className="shop-by-category">
+      <div className="categories">
+        <div className="catgeory">
+          <img src={cate1} alt="category" />
+        </div>
+        <div className="catgeory">
+          <img src={cate2} alt="category" />
+        </div>
+        <div className="catgeory">
+          <img src={cate3} alt="category" />
+        </div>
+        <div className="catgeory">
+          <img src={cate4} alt="category" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Category;
