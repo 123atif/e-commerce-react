ab8ec6bc9f9c47ab6ea86901b7c820d33bab4be1
Mohammad Atif Arshad <81535521+123atif@users.noreply.github.com>
2024-11-21 05:59:11 +0500

    working on the singleproduct page

--- client/src/components/SingleProduct/SingleProduct.jsx
+++ client/src/components/SingleProduct/SingleProduct.jsx
@@ -1,6 +1,59 @@
 import "./SingleProduct.scss";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaPinterest,
+  FaCartPlus,
+} from "react-icons/fa";
+import product_details_image from "../../assets/products/earbuds-prod-1.webp";
+import RelatedProducts from "./RelatedProducts/RelatedProducts";
+import "./SingleProduct.scss";
 const SingleProduct = () => {
-    return <div>Single Product</div>;
+  return (
+    <div className="single-product-main-content">
+      <div className="layout">
+        <div className="single-product-page">
+          <div className="left">
+            <img src={product_details_image} alt="product_details_image" />
+          </div>
+          <div className="right">
+            <span className="name">Product Name</span>
+            <span className="price">Price</span>
+            <span className="desc">Product Description</span>
+            <div className="cart-buttons">
+              <div className="quantity-button">
+                <span>-</span>
+                <span>5</span>
+                <span>+</span>
+              </div>
+              <button className="add-to-cart-button">
+                <FaCartPlus size={20} />
+                ADD TO CART
+              </button>
+            </div>
+            <span className="divider"></span>
+            <div className="info-item">
+              <span className="text-bold">
+                Category:
+                <span>Headphones</span>
+              </span>
+              <span className="text-bold">
+                Share:
+                <span className="social-icon">
+                  <FaFacebook size={16} />
+                  <FaInstagram size={16} />
+                  <FaLinkedin size={16} />
+                  <FaPinterest size={16} />
+                </span>
+              </span>
+            </div>
+          </div>
+        </div>
+        <RelatedProducts />
+      </div>
+    </div>
+  );
 };
 
 export default SingleProduct;
