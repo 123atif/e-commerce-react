6be7592dd36a233900df3178a3544435e28330f2
Mohammad Atif Arshad <81535521+123atif@users.noreply.github.com>
2024-12-05 04:44:24 +0500

    make sidebar of cart

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,10 +1,11 @@
 import { MdClose } from "react-icons/md";
 import { BsCartX } from "react-icons/bs";
+import CartItem from "./CartItem/CartItem";
 import "./Cart.scss";
 const Cart = ({ setShowCart }) => {
   return (
     <div className="cart-panel">
-      <div className="opac-layer"></div>
+      <div className="opac-layer" onClick={() => setShowCart(false)}></div>
       <div className="cart-content">
         <div className="cart-header">
           <span className="heading">Shopping Cart</span>
@@ -13,11 +14,23 @@ const Cart = ({ setShowCart }) => {
             <span className="text">Close</span>
           </span>
         </div>
-        <div className="empty-cart">
+        {/* <div className="empty-cart">
           <BsCartX size={100} />
           <span>No Product in the cart</span>
           <button className="return-cta">RETURN TO SHOP</button>
-        </div>
+        </div> */}
+        <>
+          <CartItem />
+          <div className="cart-footer">
+            <div className="subtotal">
+              <span className="text">Subtotal:</span>
+              <span className="price">PKR: 5000</span>
+            </div>
+            <div className="button">
+              <button className="checkout-cta">Checkout</button>
+            </div>
+          </div>
+        </>
       </div>
     </div>
   );
