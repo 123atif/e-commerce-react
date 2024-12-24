a4803979ca2ef0df55bbc9a35431a9450d58c1aa
Mohammad Atif Arshad <81535521+123atif@users.noreply.github.com>
2024-12-25 04:30:45 +0500

    implement search functionality

--- client/src/components/Header/Search/Search.jsx
+++ client/src/components/Header/Search/Search.jsx
@@ -1,27 +1,63 @@
 import "./Search.scss";
 import { MdClose } from "react-icons/md";
-import prod from "../../../assets/products/watch-prod-3.webp";
-
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
+import { toast } from "react-toastify";
 const Search = ({ setShowSearch }) => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const navigate = useNavigate();
+  let { data } = useFetch(
+    `/api/products?populate=*&filters[title][$contains]=${searchQuery}`
+  );
+  const handleSearch = (e) => {
+    setSearchQuery(e.target.value);
+  };
+  if (!searchQuery.length) {
+    data = null;
+  }
   return (
     <div className="search-modal">
       <div className="form-field">
-        <input type="text" autoFocus placeholder="Search for products..." />
+        <input
+          type="text"
+          autoFocus
+          placeholder="Search for products..."
+          value={searchQuery}
+          onChange={handleSearch}
+        />
 
         <MdClose onClick={() => setShowSearch(false)} />
       </div>
       <div className="search-result-content">
-        <div className="search-results">
-          <div className="search-result-item">
-            <div className="img-container">
-              <img src={prod} alt="product image" />
-            </div>
-            <div className="prod-details">
-              <span className="name">Product Name</span>
-              <span className="description">Product desc</span>
-            </div>
+        {data?.data?.length === 0 ? (
+          "No products found"
+        ) : (
+          <div className="search-results">
+            {data?.data?.map((items, id) => (
+              <div
+                className="search-result-item"
+                key={id}
+                onClick={() => {
+                  navigate(`product/${items.id}`);
+                  setShowSearch(false);
+                  toast.info("Product details");
+                }}
+              >
+                <div className="img-container">
+                  <img
+                    src={import.meta.env.VITE_DEV_URL + items?.img[0]?.url}
+                    alt="product image"
+                  />
+                </div>
+                <div className="prod-details">
+                  <span className="name">{items.title}</span>
+                  <span className="description">{items.desc}</span>
+                </div>
+              </div>
+            ))}
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
